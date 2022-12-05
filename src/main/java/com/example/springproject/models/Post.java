485a135a9a150a002169e99afddee5e010b2998b
Emirkhan03 <emirkhanismailov03@gmail.com>
2022-12-05 16:40:20 +0600

    eeeee

--- src/main/java/com/example/springproject/models/Post.java
+++ src/main/java/com/example/springproject/models/Post.java
@@ -1,9 +1,7 @@
 package com.example.springproject.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
+
 
 @Entity
 public class Post {
