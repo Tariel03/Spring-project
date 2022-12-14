712161bac25137d993c33b954253cea7379bc68a
Emirkhan03 <emirkhanismailov03@gmail.com>
2022-12-14 15:54:31 +0600

    emir changedd todos

--- src/main/java/com/example/springproject/controllers/DesignerController.java
+++ src/main/java/com/example/springproject/controllers/DesignerController.java
@@ -67,12 +67,24 @@ public String process(@PathVariable(value = "id")Long id,Model model){
         Optional<Zakaz> zakazOptional=zakazRepository.findById(id);
         if(zakazOptional.isPresent()){
             Zakaz zakaz=zakazOptional.get();
-            zakaz.setStatus("processing");
+            if (!zakaz.getStatus().equals("completed")) {
+                zakaz.setStatus("processing");
+                zakazRepository.save(zakaz);
+            }
+
+        }return "redirect:/designer";
+
+    }
+    @PostMapping("to_do/{id}")
+    public String to_do(@PathVariable(value = "id")Long id,Model model){
+        Optional<Zakaz> zakazOptional=zakazRepository.findById(id);
+        if(zakazOptional.isPresent()){
+            Zakaz zakaz=zakazOptional.get();
+            zakaz.setStatus("completed");
             zakazRepository.save(zakaz);
         }return "redirect:/designer";
 
     }
-
 
 
 
