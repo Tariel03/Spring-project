700f8b208c66ab4721d9fa001dde5f9868f07da7
erba228 <erbolzeenbekov@gmail.com>
2022-12-16 17:10:44 +0600

    erbol has

--- src/main/java/com/example/springproject/Repository/Workers_infoRepository.java
+++ src/main/java/com/example/springproject/Repository/Workers_infoRepository.java
@@ -4,9 +4,11 @@ import com.example.springproject.models.Workers_info;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface Workers_infoRepository extends JpaRepository<Workers_info, Long> {
     List<Workers_info> findWorkers_infoByavailableLike(String ava);
 
+    Optional<Workers_info>findWorkers_infoById(Long id);
 }
