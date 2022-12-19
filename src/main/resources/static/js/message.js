cc58f2b49339f9ac05002b2c4aaba02c5d2f12b5
Tariel03 <tariel.akmatov@gmail.com>
2022-12-19 08:50:47 +0600

    for emir

--- src/main/resources/static/js/message.js
+++ src/main/resources/static/js/message.js
@@ -3,14 +3,14 @@ const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 
 const BOT_MSGS = [
-    "Hi, how are you? We have such services here..."
+    "Hi, how are you? We have such services Tiktok videos, Youtube shorts, Banners, Radio ad, Tv ad and others"
 ];
 
 // Icons made by Freepik from www.flaticon.com
 const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
 const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 const BOT_NAME = "BOT";
-const PERSON_NAME = "Sajad";
+const PERSON_NAME = "You";
 
 msgerForm.addEventListener("submit", event => {
     event.preventDefault();
